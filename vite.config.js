6518ae3f847e33f8ef704f7fa43760f7ae4aa0ff
Kunal <kunal18042002@gmail.com>
2024-12-12 10:38:08 +0530

    works

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/encephalo-ai-deck/", // This should match your repository name
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
   },
+  plugins: [react()],
+  base: "/encephalo-ai-deck/", // This should match your repository name
 });
